0293601c138bcbfc107572eb67eb4ae552f0fb70
Drobotiuk Andrii <adrobotyuk@gmail.com>
2022-03-09 23:18:07 +0600

    Counter app Monitor and Button comp create

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import {Button} from './components/Button';
+import {Monitor} from './components/Monitor';
 
 function App() {
   return (
     <div className="App">
+        <Monitor/>
         <Button/>
     </div>
   );
---
+++ src/components/Monitor.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Monitor = () => {
+    return (
+        <div>
+            <h1>0</h1>
+        </div>
+    );
+};
